33e1e9cace0ea2e057dfd5081ee19049ec293421
Elias <eliasbrucart@hardgames.com.ar>
2020-04-25 00:57:13 -0300

    Part of the logic was added

--- highscore/highscore.cpp
+++ highscore/highscore.cpp
@@ -4,7 +4,12 @@ namespace scorelib
 {
 	Score::Score()
 	{
-
+		_pos = 0;
+		for (int i = 0; i < scoreSize; i++)
+		{
+			_score[i] = 0;
+			_name[i] = " ";
+		}
 	}
 
 	Score::~Score()
@@ -14,7 +19,32 @@ namespace scorelib
 
 	void Score::addPlayerToScore(string name, int score)
 	{
+		/*if (_score[_pos] == 0 && _name[_pos] == " ") 
+		{
+			_score[_pos] = score;
+			_name[_pos] = name;
+		}
+		_pos++;
+		if (_score[_pos] != 0 && _pos > 0 && _pos < 2 && _name[_pos] == " ")
+		{
+			_score[_pos] = score;
+			_name[_pos] = name;
+		}*/
 
+		for (int i = 0; i < scoreSize; i++)
+		{
+			if (_score[_pos] == i && _name[_pos] == " ")
+			{
+				_score[_pos] = score;
+				_name[_pos] = name;
+			}
+			_pos++;
+			if (_score[_pos] != i && _pos > i && _pos < i + 2 && _name[_pos] == " ")
+			{
+				_score[_pos] = score;
+				_name[_pos] = name;
+			}
+		}
 	}
 
 	void Score::removePlayerToScore(string name)
