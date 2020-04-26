085f1481bb70df6e847b9bddb1096da75a5cf6e3
Elias <eliasbrucart@hardgames.com.ar>
2020-04-26 20:50:36 -0300

    Most funcitons completed

--- highscore/highscore.cpp
+++ highscore/highscore.cpp
@@ -23,27 +23,16 @@ namespace scorelib
 
 	void Score::addPlayerToScore(string name, int score)
 	{
-		/*if (_score[_pos] == 0 && _name[_pos] == " ") 
+		if (_score[_pos] == 0 && _name[_pos] == " ")
 		{
 			_score[_pos] = score;
 			_name[_pos] = name;
 		}
-		_pos++;
-		if (_score[_pos] != 0 && _pos > 0 && _pos < 2 && _name[_pos] == " ")
-		{
-			_score[_pos] = score;
-			_name[_pos] = name;
-		}*/
 
 		for (int i = 0; i < scoreSize; i++)
 		{
-			if (_score[_pos] == i && _name[_pos] == " ")
-			{
-				_score[_pos] = score;
-				_name[_pos] = name;
-			}
 			_pos++;
-			if (_score[_pos] != i && _pos > i && _pos < i + 2 && _name[_pos] == " ")
+			if (_score[_pos] != i && _pos > i && _pos < i + 2 && _score[_pos] = 0 && _name[_pos] == " ")
 			{
 				_score[_pos] = score;
 				_name[_pos] = name;
@@ -54,6 +43,7 @@ namespace scorelib
 	void Score::removePlayerToScore(int pos)
 	{
 		_score[pos] = 0;
+		_name[pos] = " ";
 	}
 
 	void Score::getScoreList()
@@ -76,6 +66,7 @@ namespace scorelib
 		for (int i = 0; i < scoreSize; i++)
 		{
 			_score[i] = 0;
+			_name[i] = " ";
 		}
 	}
 }
